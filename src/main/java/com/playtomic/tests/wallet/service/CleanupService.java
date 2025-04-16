ff8c39e68814416c93e8110f90067a75842ac73d
Bruno Gaideski <brunogaideski@outlook.com>
2025-04-16 17:14:53 +0200

    Removing empty files

--- src/main/java/com/playtomic/tests/wallet/service/CleanupService.java
+++
@@ -1,6 +0,0 @@
-package com.playtomic.tests.wallet.service;
-
-public class CleanupService {
-    // TODO: create cron task that polls the database for ttl and take action
-    // delete older transactions
-}
